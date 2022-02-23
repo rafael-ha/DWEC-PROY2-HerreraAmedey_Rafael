9441626e77c91ed8a33787792e9ca8d7fd0808a0
rafael-ha <rherrera.alumno@iesrafaelalberti.com>
2022-02-23 23:14:47 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -1,23 +1,55 @@
-(function ($) {
-  "use strict";
-  $(window).load(function () {
-    $("#loader").fadeOut("slow", function () {
-      $("#preloader").delay(300).fadeOut("fast");
-    });
-  });
 
-  let pxShow = 300;
-  let fadeInTime = 400;
-  let fadeOutTime = 400;
-  let scrollSpeed = 300;
 
-  jQuery(window).scroll(function () {
-    if (!$("#header-search").hasClass("is-visible")) {
-      if (jQuery(window).scrollTop() >= pxShow) {
-        jQuery("#go-top").fadeIn(fadeInTime);
-      } else {
-        jQuery("#go-top").fadeOut(fadeOutTime);
-      }
-    }
-  });
+(function($) {
+
+	"use strict"; 
+   $(window).load(function() {
+    	$("#loader").fadeOut("slow", function(){
+        $("#preloader").delay(300).fadeOut("slow");
+
+      });       
+
+  	})
+
+
+  	/*---------------------------------------------------- */
+  	/* FitText Settings
+  	------------------------------------------------------ */
+  	setTimeout(function() {
+
+   	$('#intro h1').fitText(1, { minFontSize: '42px', maxFontSize: '84px' });
+
+  	}, 100);
+
+   let toggleButton = $('.menu-toggle'),
+       nav = $('.main-navigation');
+
+   // toggle button
+   toggleButton.on('click', function(e) {
+
+		e.preventDefault();
+		toggleButton.toggleClass('is-clicked');
+		nav.slideToggle();
+
+	});
+
+  	  
+
+	var pxShow = 300; 
+	var fadeInTime = 400; 
+	var fadeOutTime = 400; 
+	jQuery(window).scroll(function() {
+
+		if (!( $("#header-search").hasClass('is-visible'))) {
+
+			if (jQuery(window).scrollTop() >= pxShow) {
+				jQuery("#go-top").fadeIn(fadeInTime);
+			} else {
+				jQuery("#go-top").fadeOut(fadeOutTime);
+			}
+
+		}		
+
+	});		
+
 })(jQuery);
